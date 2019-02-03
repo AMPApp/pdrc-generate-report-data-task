f994f42c6a4d0111b5ac9ef346f2199f58b54e11
Kevin Monagle <kdmonagle@gmail.com>
2019-02-03 17:34:32 -0600

    checkpoint

--- bin/task.js
+++ bin/task.js
@@ -1,3 +1,13 @@
-/**
- * Created by kmonagle on 2/3/19.
- */
+const log = require('winston');
+const db = require('@ampapps/pdrc-db');
+require('@ampapps/pdrc-env');
+
+module.exports = async() => {
+    try {
+        await db.init();
+        log.info('Generate report data task succeeded');
+        db.close();
+    } catch (err) {
+        log.error(err);
+    }
+};
