9e1f179f4ff1e5113f3efbb45f643617ccd2092f
Kevin Monagle <kdmonagle@gmail.com>
2019-02-06 13:25:39 -0600

    added db stuff

--- bin/task.js
+++ bin/task.js
@@ -5,6 +5,35 @@ require('@ampapps/pdrc-env');
 module.exports = async() => {
     try {
         await db.init();
+        
+        const patients = await db.getAllPatients();
+
+
+        let cityData = {};
+        for(const patient of patients){
+            const patientCity = patient.city;
+            if(cityData[patientCity]){
+                cityData[patientCity] = cityData[patientCity] + 1;
+            }else{
+                cityData[patientCity] = 1;
+            }
+        }
+
+        await db.updateReportData('cityBreakdown', cityData);
+
+        let zipCodeData = {};
+        for(const patient of patients){
+            const patientzipCode = patient.zip;
+            if(zipCodeData[patientzipCode]){
+                zipCodeData[patientzipCode] = zipCodeData[patientzipCode] + 1;
+            }else{
+                zipCodeData[patientzipCode] = 1;
+            }
+        }
+
+        await db.updateReportData('zipCodeBreakdown', zipCodeData);
+
+        
         log.info('Generate report data task succeeded');
         db.close();
     } catch (err) {
